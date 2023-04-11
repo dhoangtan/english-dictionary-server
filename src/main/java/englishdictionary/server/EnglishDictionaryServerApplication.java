5895655c60cab39b70874fd42d86082f0c1a4077
danhhoangtan <danhhoangtan.it@outlook.com>
2023-04-11 10:41:54 +0700

    initialize commit

---
+++ src/main/java/englishdictionary/server/EnglishDictionaryServerApplication.java
@@ -0,0 +1,13 @@
+package englishdictionary.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishDictionaryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishDictionaryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/englishdictionary/server/EnglishDictionaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package englishdictionary.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishDictionaryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
