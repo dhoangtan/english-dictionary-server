aa88d3b5a4343c353a50278e7651cc6a42ca0560
danhhoangtan <danhhoangtan.it@outlook.com>
2023-04-28 10:33:45 +0700

    project: update dockerfile, pom.xml, README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Run project on Docker
+___
+Package the project into .jar file
+```shell
+./mvnw package
+```
+Build docker image
+```shell
+docker build . -t server:lastest
+```
+Run docker container
+```shell
+docker run -d -p 4040:8080 server:lastest
+```
--- src/main/java/englishdictionary/server/EnglishDictionaryServerApplication.java
+++ src/main/java/englishdictionary/server/EnglishDictionaryServerApplication.java
@@ -1,12 +1,28 @@
 package englishdictionary.server;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 @SpringBootApplication
 public class EnglishDictionaryServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        InputStream serviceAccount = EnglishDictionaryServerApplication.class.getResourceAsStream("/service_account_key.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
         SpringApplication.run(EnglishDictionaryServerApplication.class, args);
     }
 
