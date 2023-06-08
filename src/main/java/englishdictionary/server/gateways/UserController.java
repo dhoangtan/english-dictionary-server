f976dcc677cbd6dc6f3e665ce31361d8585c8eb6
VeeNeil <80906756+VeeNeil@users.noreply.github.com>
2023-06-08 15:45:13 +0700

    CORS

--- src/main/java/englishdictionary/server/gateways/UserController.java
+++ src/main/java/englishdictionary/server/gateways/UserController.java
@@ -16,13 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -35,6 +29,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RestController
+@CrossOrigin(origins = "http://localhost:8088")
 @RequestMapping("api/user/")
 public class UserController {
     private final verificationCodeTask verificationCodeTask;
